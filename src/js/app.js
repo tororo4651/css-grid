db44aeb2ec5c674b95b2921689508df0031bd987
tororo4651 <>
2024-08-13 20:58:02 +0900

    First Commit / Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+// Sass
+import '../scss/style.scss';
+
+
+
+
+// jQuery
+// import $ from 'jquery';
+
+// $('.contactArea').css(
+//   {
+//     color: 'green',
+//     fontSize: 32,
+//     'font-weight': 'bold',
+//     marginTop: '100px'
+//   }
+// );
+
+
+
+
+// 共通
+import './modules/common';
+
+
+
+
+// ES Modules
+// import { greet } from './modules/common';
+
+// greet('東');
---
+++ src/js/modules/common.js
@@ -0,0 +1,23 @@
+// 共通
+
+
+// const lv2Heading = document.querySelector('.lv2Heading');
+// lv2Heading.style.color = 'red';
+
+
+
+
+// jQuery
+// import $ from 'jquery';
+
+// $('.lv2Heading').css('color', 'plum');
+
+
+
+
+// 関数（あいさつ）
+// const greet = (name) => {
+//   console.log(`ありがとう。${name}さん。`);
+// };
+
+// export { greet };
